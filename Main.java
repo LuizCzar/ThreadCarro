db330c55c8a99f76b709a35661d01c3496474481
LuizCzar <kaitoyksin@gmail.com>
2025-08-08 14:53:57 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro("Carro Azul", 200); 
+        Carro carro2 = new Carro("Carro Vermelho", 300); 
+        Carro carro3 = new Carro("Carro Verde", 500); 
+
+        carro1.start();
+        carro2.start();
+        carro3.start();
+    }
+}public class Main {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro("Carro Azul", 200); 
+        Carro carro2 = new Carro("Carro Vermelho", 300); 
+        Carro carro3 = new Carro("Carro Verde", 500); 
+
+        carro1.start();
+        carro2.start();
+        carro3.start();
+    }
+}
